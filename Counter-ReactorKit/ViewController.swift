dc6b916f65d498105c61822cdaba1240eb0bfe4a
Mindohyeon <min50875@naver.com>
2022-12-05 20:44:58 +0900

    first

---
+++ Counter-ReactorKit/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Counter-ReactorKit
+//
+//  Created by 민도현 on 2022/12/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+}
+
---
+++ Counter-ReactorKitUITests/Counter_ReactorKitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Counter_ReactorKitUITestsLaunchTests.swift
+//  Counter-ReactorKitUITests
+//
+//  Created by 민도현 on 2022/12/05.
+//
+
+import XCTest
+
+final class Counter_ReactorKitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
