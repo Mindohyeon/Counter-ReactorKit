e335ef3afef7284c06676e3a19615c561d59d817
Mindohyeon <min50875@naver.com>
2022-12-11 22:29:31 +0900

    :white_check_mark: 실행 안 되던 이슈 해결
    isStubEnabled 추가

--- Counter-ReactorKitTests/Counter_ReactorKitTests.swift
+++ Counter-ReactorKitTests/Counter_ReactorKitTests.swift
@@ -9,6 +9,48 @@ import XCTest
 @testable import Counter_ReactorKit
 
 final class Counter_ReactorKitTests: XCTestCase {
+//    let sut = CounterViewController()
+    
+//    func testAction_whenDidTapIncreaseButtonDidTap_thenMutationIsIncreaseInReactor() {
+//        let counterReactor = CounterViewReactor()
+//        counterReactor.isStubEnabled = true
+//
+//        let counterViewController = sut
+////        counterViewController.loadViewIfNeeded()
+//        counterViewController.reactor = counterReactor
+//
+//        counterViewController.increaseButton.sendActions(for: .touchUpInside)
+//
+//        XCTAssertEqual(counterReactor.stub.actions.last, .increase)
+//    }
+//
+//    func testAction_whenDidTapDecreaseButtonInView_thenMutationIsDecreaseInReactor() {
+//        // Given
+//        let counterReactor = CounterViewReactor()
+//        counterReactor.isStubEnabled = true
+//
+//        let counterViewController = sut
+////        counterViewController.loadViewIfNeeded() // IBOutlet과 Action을 구성하기 위해서 호출
+//        counterViewController.reactor = counterReactor
+//
+//        // When
+//        counterViewController.decreaseButton.sendActions(for: .touchUpInside)
+//
+//        // Then
+//        XCTAssertEqual(counterReactor.stub.actions.last, .decrease)
+//    }
+    
+    func testReactor_whenExcuteIncreaseButtonTapActionInView_thenStateIsLoadingInReactor() {
+        // Given
+        let reactor = CounterViewReactor()
+        
+        // When
+        reactor.isStubEnabled = true
+        reactor.action.onNext(.increase)
+        
+        // Then
+        XCTAssertEqual(reactor.currentState.isLoading, true)
+    }
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -17,20 +59,4 @@ final class Counter_ReactorKitTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
